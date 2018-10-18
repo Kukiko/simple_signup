4f9494fe9a16b0e063ef54b591980e543177118d
Kukiko <33402648+Kukiko@users.noreply.github.com>
2018-10-18 14:03:14 +0200

    Add files via upload

---
+++ VR4.php
@@ -0,0 +1,40 @@
+<html dir=rlt>
+<head>
+<title> VR4 </title>
+</head>
+
+
+<body background= "01038685.jpg"
+BGCOLOR="000000"
+TEXT="#000066"
+LINK="#00ff00"
+VLINK="#ff0000"
+ALINK="#999999">
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><form action="login.php" method="post"/>
+<br><p align="Center"><font face="Impact"size="5"color"#FFFFFF">UserName :  <input type="text" name="UserName"/></font></p>
+<br><p align="center"><font face="Impact"size="5"color"#FFFFFF">PassWord :  <input type="PassWord" name="PassWord"/></font></p>
+<br><p Align="center"><strong><font face="Impact"size="7"><input type="submit"value="login"/></font></p>
+<br>
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+
+if ($_POST['UserName'] == 'admin' and $_POST['PassWord'] == 'admin'){
+
+  echo "Login successfull";
+
+}else{
+
+  die('login failed');
+
+}
+
+
+ ?>
+
+
+
+<html>
+
+<head>
+<title> Welcome in the My website </title>
+</head>
+<body background="01035769.jpg">
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><p align="center"><strong><font face="arial"size="7"color="000000">Welcome in the Frist website</font></strong>
+<br><p align="center"><strong><font face="arial"size="7"color="000000">Thank you for entering my first website, I hope you like it, do not forget the IK and follow up on GITHUP</font></strong>
+
+
+
+<a href ='VR4.php'> log out </a>
+
+
+
+
+
+
+
+</body>
+</html>
